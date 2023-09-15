6e86b95638b67ee7d9de86985f33296543976104
Ahmed Ali <Ahmad.20377370@compit.aun.edu.eg>
2023-09-15 05:30:34 +0300

    Accpt

---
+++ js/master.js
@@ -0,0 +1,8 @@
+$(function () {
+    'use strict';
+    $('.info-list li').click(function () {
+        $(this).addClass('active').siblings('li').removeClass('active');
+        $('.info-content div').hide();
+        $('.' + $(this).data('class')).fadeIn();
+    });
+});
